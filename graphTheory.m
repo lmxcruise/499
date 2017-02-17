74810f736378a55cc6fb2397046928bdd97ec980
Nik <Nik@192.168.56.1>
2017-02-17 01:01:59 -0600

    done bitch

--- graphTheory.m
+++ graphTheory.m
@@ -1,17 +1,20 @@
 %Graph theory version
-N = 10; %number of banks
+N = 3; %number of banks
 alpha = 1;
 monetaryReserves = 1000*ones(N,1);
 dtMonetaryReserves = zeros(N,1);
 sigma = 1;
 maxTime = 100;
 dt = 1/maxTime;
+d = zeros(N,1); % The diagonal values
+t = triu(round(rand(N)), 1); % The upper trianglar random values
+a = diag(d)+t+t' % Put them together in a symmetric matrix
 
 for t = 1:maxTime
     for i = 1:N
         rateSum = 0;
         for j = 1:N
-            rateSum = rateSum + monetaryReserves(j)-monetaryReserves(i);
+            rateSum = rateSum + a(i,j)*(monetaryReserves(j)-monetaryReserves(i));
         end;
         dW = monetaryReserves(i)*sqrt(dt)*randn;
         dtMonetaryReserves(i) = alpha/N*rateSum +sigma*dW;
