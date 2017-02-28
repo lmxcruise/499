e663cc7f5656cc3bcd22fb4b3d5d046a9a8217fa
Nik <Nik@192.168.56.1>
2017-02-28 15:11:51 -0600

    yeesg

--- ye.m
+++ ye.m
@@ -1,41 +1,65 @@
 clear;
-N = 5; %number of banks
+numBanks = 10; %number of banks
 alpha = 100;
-sigma = 1;
+sigma = 5;
+numIterations = 10000;
 maxTime = 252; %252 time step # of trading days in a year
 dt = 1/maxTime;
-monetaryReserves = zeros(N,1);
-initialCapital = 0*ones(N,1);
-dtMonetaryReserves = zeros(N,1);
-realizationReserves = zeros(N, maxTime);
+monetaryReserves = zeros(numBanks,1);
+initialCapital = 0*ones(numBanks,1);
+dtMonetaryReserves = zeros(numBanks,1);
+realizationReserves = zeros(numBanks, maxTime);
+numDefault = zeros(numBanks+1,1);
+numFailed = zeros(numIterations,1);
 
-for t = 1:maxTime
-    for i = 1:N
-        rateSum = 0;
-        for j = 1:N
-            rateSum = rateSum + (monetaryReserves(j)-monetaryReserves(i));
+for k = 1:numIterations
+    for t = 1:maxTime
+        for i = 1:numBanks
+            rateSum = 0;
+            for j = 1:numBanks
+                rateSum = rateSum + (monetaryReserves(j)-monetaryReserves(i));
+            end;
+            dW = sqrt(dt)*randn;
+            dtMonetaryReserves(i) = alpha/numBanks*rateSum*dt +sigma*dW;
         end;
-        dW = sqrt(dt)*randn;
-        dtMonetaryReserves(i) = alpha/N*rateSum*dt +sigma*dW;
+        if(t == 1)
+            monetaryReserves = dtMonetaryReserves + initialCapital;
+            realizationReserves(:,t) = monetaryReserves;
+        else 
+            monetaryReserves = dtMonetaryReserves + monetaryReserves;
+            realizationReserves(:,t) = monetaryReserves;
+        end;
     end;
-    if(t == 1)
-        monetaryReserves = dtMonetaryReserves + initialCapital;
-        realizationReserves(:,t) = monetaryReserves;
-    else 
-        monetaryReserves = dtMonetaryReserves + monetaryReserves;
-        realizationReserves(:,t) = monetaryReserves;
+    for i = 1:numBanks
+        if(monetaryReserves(i)<-0.7)
+            numFailed(k) = numFailed(k)+1;
+        end;
     end;
 end;
-monetaryReserves
-t = 1:maxTime;
-figure(1);
+for i = 1:numIterations
+   numDefault(numFailed(i)+1) = numDefault(numFailed(i)+1) + 1;
+end;
 
-for i =1:N
-    plot(t,realizationReserves(i,:));
-    hold on;
-end;
-title(['Alpha = ', num2str(alpha), ' realization']);
-xlabel('Time');
-xlim([0 252]);
-ylim([-3 3]);
+monetaryReserves
+plot(numDefault/numIterations);
+title(['Alpha = ', num2str(alpha)]);
+xlabel('Number of Default');
+% xlim([0 numBanks])
+numDefault
+
+
+
+% t = 1:maxTime;
+% figure(1);
+
+
+% A realization of bank reserves
+% for i =1:N
+%     plot(t,realizationReserves(i,:));
+%     hold on;
+% end;
+% title(['Alpha = ', num2str(alpha), ' realization']);
+% xlabel('Time');
+% xlim([0 252]);
+% ylim([-3 3]);
 
